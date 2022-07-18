5b1a455a6f9dc0e8c4d05b5a59b1d3de0684d448
jucelio coelho <juceliocoelho2010@gmail.com>
2022-07-18 16:19:03 -0300

    alura-aula1

---
+++ src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        // fazer uma conexão HTTP e buscar os top 250 filmes
+
+        //extrair só os dados que interessam (titulo, poster, classificação)
+        
+    }
+}
